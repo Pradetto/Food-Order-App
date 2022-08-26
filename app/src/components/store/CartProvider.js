6cf9491b88eedbaf4b942fb294ebe4e892dfadab
Michael Pradetto <pradetto5@gmail.com>
2022-08-26 16:55:51 -0700

    App complete

--- app/src/components/store/CartProvider.js
+++ app/src/components/store/CartProvider.js
@@ -30,11 +30,30 @@ const cartReducer = (state, action) => {
       updatedItem = { ...action.item };
       updatedItems = state.items.concat(action.item); // don't use push because it would edit the existing array you use concat because it returns a new array and you want to update state in an inmuttable way
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
-  } else if (action.type === "REMOVE") {
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
   }
   return defaultCartState;
 };
